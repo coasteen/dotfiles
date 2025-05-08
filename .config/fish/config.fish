909ccc19013c5b6a4ae06a788e89da03f90bb374
coast <coast@noreply.localhost>
2025-05-09 00:43:18 +0200

    Update .config/fish/config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,41 +1,47 @@
 if status is-interactive
-alias v="vim"
-alias vi="vim"
+alias emacs="swallow emacs"
 alias ri="ranger"
 alias rim="ranger"
 
+alias man="batman"
 alias cat="bat"
-#alias ls="eza --icons"
+alias ls="eza --icons"
 
-alias minecraft="prime-run java -jar /home/coast/documents/TLauncher.jar"
+alias minecraft="prime-run java -jar /home/coast/Documents/TLauncher.jar"
 
-export EDITOR=vim
+export EDITOR=emacs
 
 alias cdwm="cd ~/.suckless/dwm && doas vim config.h; cd -"
-alias cpicom="vim ~/.config/picom/picom.conf"
+alias cpicom="emacs ~/.config/picom/picom.conf"
 alias mdwm="cd ~/.suckless/dwm && doas make install && cd -"
-alias cneo="vim ~/.config/neofetch/con*"
+alias cneo="emacs ~/.config/neofetch/con*"
 
+function vim() {
+    echo "Error: use emacs. you have to."
+}
+end
 function sudo() {
     echo "Error: fuck you don't use this command"
 }
 end
 function fish_prompt
     set_color fe8019
-    printf '[ '
+    printf '['
     set_color yellow
     printf '%s' (whoami)
     set_color white
     printf '@'
     set_color cyan
-    printf 'coasteen'
+    printf 'coasteen '
     set_color white
-    printf ' in '
+    #printf ''
     set_color brblue
     printf '%s' (prompt_pwd)
     set_color fe8019
-    printf ' ] '
+    printf ']$ '
     set_color normal
 end
 set -g fish_greeting
 end
+set -U fish_color_autosuggestion '808080'
+bash
